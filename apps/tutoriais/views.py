d10be07867ef6421b403da0d081e39db5b30d59e
Hugo Ferreira <hugorobertiferreira61.br@gmail.com>
2024-05-08 14:42:20 -0300

    Atualizando funções
    Atualizando funções

--- apps/tutoriais/views.py
+++ apps/tutoriais/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from apps.tutoriais.models import cadastroTutoriais
 
 def tutoriais(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect('login')
+    
     tutoriais = cadastroTutoriais.objects.order_by('titulo').filter(publicada=True)
     return render(request, 'tutoriais/tutoriais.html', {"tutoriais": tutoriais})
 
 
 def visualizar(request, tutorial_id):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect('login')
+    
     texto = []
     tutorial = get_object_or_404(cadastroTutoriais, pk=tutorial_id)
     caminho_arquivo = tutorial.tutorial.path
